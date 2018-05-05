c8dddabce0d8d759d87669740a03edae2f038afe
Zames Chua <zames.chua@gmail.com>
2018-05-05 23:48:06 +0800

    Add basic authentication

--- app.js
+++ app.js
@@ -1,14 +1,113 @@
 const express = require('express');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const path = require('path')
+const User = require('./models/User');
 
-// Constants
 const PORT = process.env.PORT || 8080;
+
+//connect to MongoDB
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://db/test');
+const db = mongoose.connection;
+
+//handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('MongoDB connected!');
+});
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password'
+  },
+  function(email, password, done) {
+    console.log("in passport.use");
+    User.findOne({ email: email }, function (err, user) {
+      /*
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      */
+      return done(null, user);
+    });
+  }
+));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  User.findOne({_id: id}, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
 
 // App
 const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('cookie-parser')());
 
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Routes
 app.get('/', function (req, res) {
-  res.send('Hello world!!!\n');
+  res.render('index.html');
 })
+
+app.get('/register', function (req, res) {
+  res.render('register.html');
+})
+
+app.post('/register', function(req, res) {
+  if (req.body.email &&
+    req.body.password &&
+    req.body.password === req.body.passwordConfirm) {
+    const userData = {
+      email: req.body.email,
+      password: req.body.password,
+    }
+    User.create(userData, function (err, user) {
+      if (err) {
+        // HANDLE ERRORS HERE LATER
+        console.log(err);
+        return err;
+      } else {
+        return res.redirect('/dashboard');
+      }
+    });
+  }
+})
+
+app.get('/signin', function(req, res) {
+    res.render('signin.html') 
+  } 
+);
+
+app.post('/signin',
+    passport.authenticate('local', { failureRedirect: '/signin' }),
+    function(req, res) {
+      // If this function gets called, authentication was successful.
+      // `req.user` contains the authenticated user.
+      res.redirect('/dashboard/' + req.user.email);
+});
+
+app.get('/dashboard', function(req, res) {
+  res.render('dashboard.html') 
+});
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
 
 app.listen(PORT)
 console.log('Running on http://localhost:' + PORT);
---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+});
+
+UserSchema.methods.verifyPassword = function(password, callback) {
+  console.log("verifying pw in userschema");
+  callback(err, bcrypt.compareSync(password, this.password));
+};
+  
+//hashing a password before saving it to the database
+UserSchema.pre('save', function(next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash) {
+    if (err) {
+    return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
